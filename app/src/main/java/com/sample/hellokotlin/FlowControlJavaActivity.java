059ff2c02d0e44fe407515d58012f9ba61c23a07
HyeJin <rkd2313@gmail.com>
2021-05-04 10:14:19 +0900

    Flow Control in Java

--- app/src/main/java/com/sample/hellokotlin/FlowControlJavaActivity.java
+++ app/src/main/java/com/sample/hellokotlin/FlowControlJavaActivity.java
@@ -38,11 +38,7 @@ public class FlowControlJavaActivity extends AppCompatActivity {
                 switch (number){
                     case 4:
                     case 9:
-                        if(number == 4) {
-                            btnLD.setText("실행 for 4");
-                        }
-                        else
-                            btnLD.setText("실행 for 9");
+                            btnLD.setText("실행 for" + number);
                         break;
                     default:
                         btnLD.setText("실행 for else"); break;
